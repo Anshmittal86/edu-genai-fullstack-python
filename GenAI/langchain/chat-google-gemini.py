54117f7578e4d71108e04c22c27d2949a8bbd1c5
Ansh Mittal <anshmit657@gmail.com>
2026-06-02 15:24:24 +0530

    refactor: add comment

--- GenAI/langchain/chat-google-gemini.py
+++ GenAI/langchain/chat-google-gemini.py
@@ -3,21 +3,20 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+# Initialize the model
+model = ChatGoogleGenerativeAI(model="gemini-3.5-flash")
 
-model = ChatGoogleGenerativeAI(
-    model="gemini-3.5-flash"
-)
-
+# Messages History
 messages = [
     ("system", "You are expert email writer."),
     ("human", "Write a 200 word email for wishing happy birthday."),
 ]
 
-# Without stream 
+# print response without streaming
 response = model.invoke(messages)
 print(response.text)
 
 
-# With streaming 
-for chunk in model.stream(messages):
-    print(chunk.text)
+# print response with streaming 
+# for chunk in model.stream(messages):
+#     print(chunk.text)
